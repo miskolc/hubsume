05b0f721164b37ece8e03c3d49f1d447c9e1f765
Teodor Pripoae <teodor.pripoae@gmail.com>
2013-01-28 23:28:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hubsume::Application.config.secret_key_base = '04caa7cb9bfb671c9947abfeda10bab393c49f345c6f27a356910d75db7dd443a2519782651365341887ec14139e10fba954177e040bad520726700f6608c748'
